3bae511eedda607b00faf09e98030b9eaf3c5051
rakibhossainofficial <rakib20@gmail.com>
2024-10-09 21:18:22 +0600

    Header Responsive Done

--- Responsive-Gym-Website-main/assets/js/script.js
+++ Responsive-Gym-Website-main/assets/js/script.js
@@ -4,11 +4,36 @@
 
 // ---------- Add Event On Element
 
-
-
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback);
+    }
+}
 
 
 // ---------- Navbar Toggle
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active") 
+    
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+    navbar.classList.remove("active");
+}
+
+addEventOnElem(navLinks, "click", closeNavbar);
 
 
 
