381950f12ebc44a66f2ea8e2ed16e3a558a732de
rakibhossainofficial <rakib20@gmail.com>
2024-10-11 21:14:40 +0600

    Back to Top Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,22 @@ addEventOnElem(navLinks, "click", closeNavbar);
 
 
 // ---------- Header and Back Top Btn Active
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function(){
+    if (this.window.scrollY > 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active")
+    }
+})
+
+
+
+
+
+
